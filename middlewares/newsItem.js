e910f25346041680cbf99abfe28dbb4427044617
Jerry Liu <ljliu89@gmail.com>
2024-10-31 22:37:40 -0700

    setup file structure

---
+++ middlewares/newsItem.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const { validate } = require("./user");
+
+const newsItemSchema = new mongoose.Schema({
+  keyword: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  source: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must enter a valid URL",
+    },
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must enter a valid URL",
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+    select: false,
+  },
+});
+
+module.exports = mongoose.model("item", newsItemSchema);
---
+++ middlewares/user.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const validator = require("validatoe");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator(value) {
+        return validator.isEmail(value);
+      },
+      message: "You must enter a valid email",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+module.exports = mongoose.model("user", userSchema);
