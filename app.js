94c3ea36b4db86649891afebfe3a84915cf35087
Jerry Liu <ljliu89@gmail.com>
2024-12-03 23:38:23 -0800

    Add middlewares and error handlers

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const {errors} = require("celebrate")
 
 const mainRouter = require("./routes/index");
 const { createUser, login } = require("./controllers/users");
 const { getNewsItem } = require("./controllers/newsItems");
+const auth = require("./middlewares/auth");
+const errorHandler = require("./middlewares/error-handler");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const app = express();
 
@@ -13,11 +17,18 @@ const { PORT = 3001 } = process.env;
 app.use(express.json());
 app.use(cors());
 
+app.use(requestLogger);
 app.post("/signup", createUser);
 app.post("/signin", login);
 app.get("/articles", getNewsItem);
 
+app.use(auth);
 app.use("/", mainRouter);
+
+app.use(errorLogger);
+
+app.use(errors())
+app.use(errorHandler);
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/newsexplorer_db")
---
+++ utils/errors.js
@@ -0,0 +1,8 @@
+module.exports = {
+  invalidDataError: 400,
+  authenticationError: 401,
+  forbiddenError: 403,
+  notFoundError: 404,
+  conflictError: 409,
+  defaultError: 500,
+};
